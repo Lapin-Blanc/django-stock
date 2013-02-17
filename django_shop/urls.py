0263d15515382548a158deb1a637529610e9b28e
Lapin-Blanc <github@lapin-blanc.com>
2013-02-17 10:38:47 +0100

    added article search based on scanned barcode

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
     
     # stock
     url(r'^stock/article/(?P<object_id>\d+)/$', object_detail, {'queryset' : Article.objects.all()}),
+    url(r'^stock/article/search/(?P<article_ean>|\d{6,})$', "stock.views.find_article"),
+    url(r'^stock/article/remove_article/$', "stock.views.remove_article"),
     # docuemnts
     url(r'^documents/barcodepage/(?P<page_id>\d+)/$', 'documents.views.print_barcode_page'),
 )
--- stock/views.py
+++ stock/views.py
@@ -1,1 +1,31 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from stock.models import Article
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.http import Http404, HttpResponseRedirect
+
+def find_article(request, article_ean):
+    if article_ean:
+        try:
+            article = get_object_or_404(Article, ean=article_ean)
+        except Http404:
+            article = None
+    else:
+        article = None
+    return render(request, "stock/search_article.html", {
+                                     "article" : article
+                                     },
+                             )
+
+def remove_article(request):    
+    if request.POST:
+        try:
+            article = Article.objects.get(ean=request.POST["ean"][:-1])
+            #article.stock -= 1
+            #article.save()
+            return HttpResponseRedirect(reverse("stock.views.find_article", kwargs={"article_ean": article.ean }))
+        except (KeyError, Article.DoesNotExist):
+            return HttpResponseRedirect(reverse("stock.views.find_article", kwargs={"article_ean":""}))
+    else:
+        return HttpResponseRedirect(reverse("stock.views.find_article", kwargs={"article_ean":""}))
+
